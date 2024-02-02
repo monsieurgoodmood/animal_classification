b5921339ee8e8e5617b3e837bbf0e7d557319b7f
Arthur Choisnet <arthur.choisnet74@gmail.com>
2024-02-02 19:50:53 +0100

    training model

--- model.py
+++ model.py
@@ -7,4 +7,4 @@ model = YOLO('yolov8n-cls.pt')  # Le modèle sera téléchargé automatiquement 
 results = model.train(data='/content/animal_classification/animal_data.yaml', # Chemin vers votre fichier YAML
                       epochs=50,  # Nombre d'époques pour l'entraînement
                       imgsz=224,  # Taille des images pour l'entraînement
-                      batch_size=16)  # Taille du lot pour l'entraînement
+                      batch=16)  # Utilisez 'batch' au lieu de 'batch_size'
