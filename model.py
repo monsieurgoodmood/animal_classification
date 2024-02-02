f10bfb6d0239926364cef3101487c61f1334bb3f
Arthur Choisnet <arthur.choisnet74@gmail.com>
2024-02-02 20:14:22 +0100

    training model

--- model.py
+++ model.py
@@ -1,8 +1,8 @@
 from ultralytics import YOLO  # Importez la classe YOLO
 
 # Assurez-vous que le modèle et le fichier de données YAML sont spécifiés correctement
-model = YOLO('yolov8n-cls.pt')  # Spécifiez le chemin correct au modèle pré-entraîné si nécessaire
+model = YOLO('yolov8n-cls.yaml')  # Spécifiez le chemin correct au modèle pré-entraîné si nécessaire
 
 # Lancer l'entraînement
 # Notez que vous devez vous assurer que 'data' pointe vers le chemin correct de votre fichier YAML
-results = model.train(data='/content/animal_classification/datasets/animal_data.yaml', epochs=50, imgsz=224, batch=16)
+results = model.train(data='/content/animal_classification/animal_data.yaml', epochs=50, imgsz=224, batch=16)
